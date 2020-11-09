16e317e3d558d30c8142ded10554bc64c8b357d5
Gabriel Eduardo Acosta Nuñez <gacosta@confisa.do>
2020-11-08 23:53:53 -0400

    DbContext Configuration

---
+++ Tienda.Common/Tienda.Common/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Tienda.Common.Entities
+{
+   public class Country
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+
+}
--- Tienda.Common/Tienda.Web/Startup.cs
+++ Tienda.Common/Tienda.Web/Startup.cs
@@ -7,8 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols;
+using Tienda.Web.Data;
 
 namespace Tienda.Web
 {
@@ -31,6 +34,10 @@ namespace Tienda.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
